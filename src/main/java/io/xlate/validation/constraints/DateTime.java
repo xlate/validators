9163a96ae289f379c0cd22df9e27d13df179cf8d
Michael Edgar <michael@xlate.io>
2017-07-22 20:16:47 -0400

    Rename package constraintvalidators to constraintvalidation

--- src/main/java/io/xlate/validation/constraints/DateTime.java
+++ src/main/java/io/xlate/validation/constraints/DateTime.java
@@ -29,7 +29,7 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-import io.xlate.validation.constraintvalidators.DateTimeValidator;
+import io.xlate.validation.constraintvalidation.DateTimeValidator;
 
 /**
  * The annotated element value must contain characters valid for the given
