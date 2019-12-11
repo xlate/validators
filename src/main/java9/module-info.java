4ecd710506045d79bcc62e6eeda7ad5bcd943894
Michael Edgar <michael@xlate.io>
2019-12-10 21:59:48 -0500

    Update dependencies and plugins
    Signed-off-by: Michael Edgar <michael@xlate.io>

---
+++ src/main/java9/module-info.java
@@ -0,0 +1,13 @@
+/*module io.xlate.validation {
+    exports io.xlate.validation.constraints;
+
+    requires jakarta.el.api;
+    requires java.instrument;
+    requires java.logging;
+    requires java.management;
+    requires java.naming;
+    requires java.prefs;
+    requires java.sql;
+    requires java.sql.rowset;
+    requires java.validation;
+}*/
